366efb7c089e4eb96f638753c2b4a83ce16c78a6
施帅帅 <17858024722@163.com>
2018-06-21 15:50:25 +0800

    no message

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,14 +7,14 @@ use think\Controller;
 class Index extends controller
 {
     //定义控制器初始化方法_initialize，在该控制器的方法调用之前首先执行。
-    public function _initialize()
+/*    public function _initialize()
     {
         if(!session('email')){
-            $this->redirect('land');
+            $this->redirect('admin/index');
         }
-    }
+    }*/
     public function index()
     {
-        return view('/houtai');
+        return view('/index/houtai');
     }
 }
